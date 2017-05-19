afa222252879ae31adaf80a9094bc1f20ef9dd09
Rafael Garcia <rafael.garcia@vibes.com>
2017-05-19 14:20:16 -0400

    [#14] Configure Comfortable Mexican Sofa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  comfy_route :cms_admin, :path => '/admin'
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
