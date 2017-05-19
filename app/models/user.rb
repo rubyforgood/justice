5ad521bd413b25f74f5cd55a9cf6cb65720b4ecd
Emily Bosakowski <emily.bosakowski@vibes.com>
2017-05-19 13:49:31 -0400

    Add Devise to Gemfile, create user model, configure for devise, add devise views

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # TODO: decide which Devise modules we do and don't need
+  # YES to database_authenticatable, validateable, recoverable
+  # MAYBE registerable, rememberable,  confirmable
+  # NO PROBABLY trackable,
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # TODO: set :host in environments/production.rb to be the domain of our production app
+  config.action_mailer.default_url_options = { :host => 'justice.dev' }
 end
