7af2ee34457adafead2758ea21be33d5c0555005
Jennifer Konikowski <jmkoni@icloud.com>
2017-05-20 11:37:03 -0400

    Update schema.rb with appropriate version number

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_520_135_607) do
+ActiveRecord::Schema.define(version: 20170520135607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
