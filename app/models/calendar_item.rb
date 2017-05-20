41845f33b67ce094e29fd5626ca630bb81e3207d
alitopaloglu <ali_t@gwmail.gwu.edu>
2017-05-20 10:23:19 -0400

    adding calendar view actions

--- app/models/calendar_item.rb
+++ app/models/calendar_item.rb
@@ -1,7 +1,10 @@
 class CalendarItem < ApplicationRecord
   validates_presence_of :title, :start_time
 
+<<<<<<< HEAD
   def start_time
     self.calendar_item.start
   end
+=======
+>>>>>>> adding calendar view actions
 end
