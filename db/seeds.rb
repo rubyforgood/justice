5628c00483524e7108c7b26914292e21593e3207
SuperJones <anissajones10@gmail.com>
2017-05-20 15:31:51 -0400

    Fixes styling in Lesson#index/show and seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+10.times do |n|
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "person#{n}@#{Faker::Internet.domain_name}",
+    password: "password",
+    password_confirmation: "password",
+    sign_in_count: 1
+  )
+end
+
 10.times do
   Term.create(name: Faker::Hipster.word)
 end
@@ -17,8 +28,10 @@ end
     lesson_type: Lesson::LESSON_TYPES.sample,
     title: Faker::Book.title,
     body:  Faker::Hipster.paragraph,
-    questions: Faker::Hipster.sentence
+    questions: Faker::Hipster.sentence,
+    user: User.order('RANDOM()').take
   )
+
   3.times do
     lesson.lesson_terms.create(term: Term.order('RANDOM()').take)
   end
