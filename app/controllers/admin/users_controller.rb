a8aca668a626083af1b9e1c746bd374cd9c069f1
Rafael Garcia <r@ragar.me>
2017-06-03 04:51:26 -0500

    Users can now be updated without having to set a password. Approved and suspended flags can also be updated

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -29,12 +29,13 @@ class Admin::UsersController < Comfy::Admin::Cms::BaseController
   end
 
   def update
-    @user.update_attributes!(user_params)
+    the_user_params = user_params[:password].present? ? user_params : user_params.except(:password)
+    @user.update_attributes!(the_user_params)
     flash[:success] = 'User updated'
-    redirect_to :action => :show, :id => @user
+    redirect_to [:admin, @user], notice: 'User was successfully updated.'
   rescue ActiveRecord::RecordInvalid
-    flash.now[:danger] = 'Failed to update User'
-    render :action => :edit
+    flash.now[:danger] = 'Failed to update user'
+    render :edit
   end
 
   def destroy
@@ -57,6 +58,6 @@ protected
   end
 
   def user_params
-    params.fetch(:user, {}).permit(:first_name, :last_name, :email, :status)
+    params.fetch(:user, {}).permit(:first_name, :last_name, :email, :password, :approved, :suspended)
   end
 end
